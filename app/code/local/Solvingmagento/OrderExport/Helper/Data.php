c4e89bf5bf8fc83c1b4a8f451e9de0ce10737ab5
Amzad Mojumder <amojumder@ryman.co.uk>
2014-04-10 14:47:46 +0100

    created Helper/Data.php

---
+++ app/code/local/Solvingmagento/OrderExport/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AMojumder
+ * Date: 10/04/14
+ * Time: 14:47
+ */
+
+class Solvingmagento_OrderExport_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
