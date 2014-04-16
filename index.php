87febced38d6d0a430c0c5562df58dfd5a31dfb9
Amzad Mojumder <amojumder@ryman.co.uk>
2014-04-16 11:03:57 +0100

    removed Serilization

--- index.php
+++ index.php
@@ -90,5 +90,6 @@ $mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : ''
 $mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
 
 
+Mage::log('TEst Data');
 
 Mage::run($mageRunCode, $mageRunType);
