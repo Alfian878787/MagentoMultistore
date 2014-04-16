accf2ee25ab96425a3a0c1fab02747c33c554bb1
Amzad Mojumder <amojumder@ryman.co.uk>
2014-04-16 11:05:19 +0100

    Removed Serilization 01

---
+++ app/code/local/Solvingmagento/OrderExport/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AMojumder
+ * Date: 15/04/14
+ * Time: 14:30
+ */
+
+class Solvingmagento_OrderExport_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
